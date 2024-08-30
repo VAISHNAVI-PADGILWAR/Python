e89e4bc6068948d22bf38a3f6bc16a1b26d3ee4c
VAISHNAVI-PADGILWAR <129828855+VAISHNAVI-PADGILWAR@users.noreply.github.com>
2024-08-30 22:59:22 +0530

    Create IsGreaterThan.py

---
+++ IsGreaterThan.py
@@ -0,0 +1,7 @@
+num1 = float(input("Enter your first number: "))
+num2 = float(input("Enter your second number: "))
+
+print(num1 > num2)
+#This will return me the boolean value if the number is greater or not
+#if == is needed to be checked we can use conditional statements 
+#This is just a basic program!
