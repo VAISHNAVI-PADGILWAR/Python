9b8d5ea49c98cdc61e437a56b8a4e5db05983eca
VAISHNAVI-PADGILWAR <129828855+VAISHNAVI-PADGILWAR@users.noreply.github.com>
2024-08-30 22:52:36 +0530

    Update AvgOfTwo.py

--- AvgOfTwo.py
+++ AvgOfTwo.py
@@ -1,5 +1,5 @@
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
 
 avg = (num1 + num2)/2
 print("The average of numbers entered is: ", avg)
