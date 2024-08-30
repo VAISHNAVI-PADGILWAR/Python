632b70eb2ddb9fa6c4d032c4cbe7773d03d0cee8
VAISHNAVI-PADGILWAR <129828855+VAISHNAVI-PADGILWAR@users.noreply.github.com>
2024-08-30 23:14:11 +0530

    Create countChar.py
    #This is a basic Python Program to count number of occurrences of a substring using count() function

---
+++ countChar.py
@@ -0,0 +1,8 @@
+#This is a basic Python Program to count number of occurances of a substring using count() function
+
+str = input("Enter a string: ")
+substr = input("Enter the substring: ")
+# str = str.lower()
+print(str.count(substr))
+
+#If you want to count number of occurances of a substring irrespective of its case you can use lower() function
