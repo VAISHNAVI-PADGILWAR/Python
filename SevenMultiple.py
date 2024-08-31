037ba9283daeb115ddb4fdb71d1ce678dce465b7
VAISHNAVI-PADGILWAR <129828855+VAISHNAVI-PADGILWAR@users.noreply.github.com>
2024-08-31 23:10:55 +0530

    Create SevenMultiple.py
    This is a simple Python Program to check whether a number is multiple of 7 or not.

---
+++ SevenMultiple.py
@@ -0,0 +1,9 @@
+num = int(input("Enter an integer: "))
+
+def SevenMultiple(num):
+    if num % 7 == 0:
+        print("The number is a multiple of 7")
+    else:
+        print("The number is not a multiple of 7")
+
+SevenMultiple(num)
