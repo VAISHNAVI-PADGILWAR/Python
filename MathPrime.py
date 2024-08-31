2ffe17cd179faa4c83772b91ee70f3dfc178a431
VAISHNAVI-PADGILWAR <129828855+VAISHNAVI-PADGILWAR@users.noreply.github.com>
2024-08-31 16:21:47 +0530

    Create MathPrime.py
    This is a simple Python Program to check whether a given number is Prime or Composite using math sqrt function which is an even more optimized code.

---
+++ MathPrime.py
@@ -0,0 +1,18 @@
+import math
+
+num = int(input("Enter a number: "))
+
+def PrimeOrNot(num):
+    flag = 1
+    if num >1:
+        for i in range(2, int(math.sqrt(num))+1):
+            if num % i == 0:
+                flag = 0
+        if flag == 1:
+            print("The number is Prime")
+        else:
+            print("The number is Composite")
+    else:
+        print("Enter a number greater than 1")
+
+PrimeOrNot(num)
